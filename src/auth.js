e16d47db848bf1c8a8769aa3a7763358b1da620a
jackie <kr0106686@gmail.com>
2024-05-12 21:06:41 +0900

    테스트 변경

--- src/auth.js
+++ src/auth.js
@@ -1,12 +1,12 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
-import Google from "@auth/sveltekit/providers/google";
-import Naver from "@auth/sveltekit/providers/naver";
+// import Google from "@auth/sveltekit/providers/google";
+// import Naver from "@auth/sveltekit/providers/naver";
 
 const providers = [
-    Google,
+    // Google,
     GitHub,
-    Naver
+    // Naver
 ]
 
 export const { handle, signIn, signOut  } = SvelteKitAuth({
--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    csrf: {
-      checkOrigin: false
-    },
     adapter: adapter()
   },
   preprocess: vitePreprocess()
