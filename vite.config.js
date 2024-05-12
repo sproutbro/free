9ba583ad3002faddd33c91eb70f8a32939d8b848
jackie <kr0106686@gmail.com>
2024-05-12 22:43:09 +0900

    ssl 인증서 업데이트

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		https: {
+			key: fs.readFileSync('/etc/letsencrypt/live/diving.run/fullchain.pem'),
+			cert: fs.readFileSync('/etc/letsencrypt/live/diving.run/privkey.pem'),
+		}
+	}
 });
