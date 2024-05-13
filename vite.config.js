42ad60151062dcf2e57221a6dcf40c7d5062e2c0
sprout <krpsroutbro@gmail.com>
2024-05-13 23:04:41 +0900

    dddd

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import fs from 'fs';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		https: {
-			key: fs.readFileSync('/etc/letsencrypt/live/diving.run/fullchain.pem'),
-			cert: fs.readFileSync('/etc/letsencrypt/live/diving.run/privkey.pem'),
-		}
-	}
+	plugins: [sveltekit()]
 });
