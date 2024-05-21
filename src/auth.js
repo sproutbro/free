900964ddccf6b52b4562a4808a2a5fbf44e596d5
sprout <krpsroutbro@gmail.com>
2024-05-21 13:31:57 +0900

    auth modify

--- src/auth.js
+++ src/auth.js
@@ -12,7 +12,6 @@ const providers = [
 
 export const { handle, signIn, signOut} = SvelteKitAuth({
     providers,
-    providers,
     trustHost: true,
     theme: {
         logo: "https://next-auth.js.org/img/logo/logo-xs.png",
